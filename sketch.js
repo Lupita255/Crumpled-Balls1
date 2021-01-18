401ff4faaa4e17d7dc228a9063528dca653fdcb4
Lupita255 <75394038+Lupita255@users.noreply.github.com>
2021-01-18 15:44:48 -0600

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,50 +1,55 @@
+
 const Engine = Matter.Engine;
 const World = Matter.World;
 const Bodies = Matter.Bodies;
 const Body = Matter.Body;
-var engine;
-var paper;
-var bin1, bin2, bin3;
 var ground;
+var papers;
+var bin1, bin2, bin3;
+var bin, binImg;
 
-function preload() {}
+function preload(){}
 
 function setup() {
-  createCanvas(800, 700);
+	createCanvas(1100,700);
 
-  engine = Engine.create();
-  world = engine.world;
 
-  //Create the Bodies Here.
- paper = new Paper(150,200,50,50);
+	engine = Engine.create();
+	world = engine.world;
 
- bin1 = new Bin(840,660,230,20);
- bin2 = new Bin(715,605,20,130);
- bin3 = new Bin(965,605,20,130);
+	//Create the Bodies Here.
+	paper = new Paper(150,200,50,50);
 
- ground = new Ground(500,680,1200,20);
+	bin1 = new Bin(840,660,230,20);
+	bin2 = new Bin(715,605,20,130);
+	bin3 = new Bin(965,605,20,130);
 
-  Engine.run(engine);
+	ground = new Ground(500,680,1200,20);
+
+
+	Engine.run(engine);
+  
 }
+
 
 function draw() {
   rectMode(CENTER);
   background(0);
-	
-  Engine.update(engine);
-	
+
   paper.display();
   bin1.display();
   bin2.display();
   bin3.display();
   ground.display();
-	
+
   keyPressed();
 
   drawSprites();
+ 
 }
+
 function keyPressed() {
-  if (keyCode === UP_ARROW) {
-    Matter.Body.applyForce(paper.body, paper.body.position, { x: 6, y: -6 });
+	if (keyCode === UP_ARROW) {
+	  Matter.Body.applyForce(paper.body, paper.body.position, {x:6, y:-6});
+	}
   }
-}
