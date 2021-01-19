8356ac3a6e895c3143c4eb313443ad98960b7650
Lupita255 <75394038+Lupita255@users.noreply.github.com>
2021-01-19 16:17:49 -0600

    Update paper.js

--- paper.js
+++ paper.js
@@ -15,5 +15,11 @@ World.add(world,this.body);
 display(){
     var paperpos=this.body.position
     ellipse(0,0,this.r,this.r);
+    push()
+    translate(paperpos.x, paperpos.y); 
+    rectMode(CENTER) strokeWeight(3); 
+    fill(255,0,255) 
+    ellipse(0,0,this.r, this.r); 
+    pop()
   }
 }
