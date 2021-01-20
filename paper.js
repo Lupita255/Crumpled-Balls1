f05a0eda004e87d243194e90968a7ef6b19e266e
Lupita255 <75394038+Lupita255@users.noreply.github.com>
2021-01-20 09:59:53 -0600

    Update paper.js

--- paper.js
+++ paper.js
@@ -17,7 +17,8 @@ display(){
     ellipse(0,0,this.r,this.r);
     push()
     translate(paperpos.x, paperpos.y); 
-    rectMode(CENTER) strokeWeight(3); 
+    rectMode(CENTER) 
+    strokeWeight(3); 
     fill(255,0,255) 
     ellipse(0,0,this.r, this.r); 
     pop()
